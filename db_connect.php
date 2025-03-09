71d619c60fdede384f35515411b05204d772c2b1
Ruchirani-1999 <ruchiranidakshina@gmail.com>
2025-03-09 10:51:52 +0530

    .env and .gitignore

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "private_campus";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
